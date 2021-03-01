ec47e9e80a1349de0aed8865f3cf2dda8b6cfa76
Kelvin Ortiz <36288868+iopkelvin@users.noreply.github.com>
2021-02-28 19:54:58 -0800

    bugs

--- src/resources/store.py
+++ src/resources/store.py
@@ -1,6 +1,7 @@
 from flask_restful import Resource
 from models.store import StoreModel
 
+
 class Store(Resource):
     def get(self, name):
         store = StoreModel.find_by_name(name)
@@ -30,4 +31,5 @@ class Store(Resource):
 
 class StoreList(Resource):
     def get(self):
-        return {'stores': list(map(lambda x: x.json(), StoreModel.query.all()))}
+        # return {'stores': list(map(lambda x: x.json(), StoreModel.query.all()))}
+        return {'stores': [store.json() for store in StoreModel.query.all()]}
