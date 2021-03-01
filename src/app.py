63264ee196534544842da7ff96bf28b24d92ca0d
Kelvin Ortiz <36288868+iopkelvin@users.noreply.github.com>
2021-02-28 20:24:12 -0800

    bug

--- src/app.py
+++ src/app.py
@@ -23,6 +23,7 @@ api.add_resource(Store, '/store/<string:name>')  # http://127.0.0.1:5000/item/..
 api.add_resource(StoreList, '/stores')
 api.add_resource(Item, '/item/<string:name>')
 api.add_resource(ItemList, '/items')
+
 api.add_resource(UserRegister, '/register')
 
 if __name__ == '__main__':
