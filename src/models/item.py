87eb9da6ad3f2fc7cd3fe4648815d849201732db
Kelvin Ortiz <36288868+iopkelvin@users.noreply.github.com>
2021-02-28 17:23:36 -0800

    added new information to improve models

--- src/models/item.py
+++ src/models/item.py
@@ -8,9 +8,13 @@ class ItemModel(db.Model):
     name = db.Column(db.String(80))
     price = db.Column(db.Float(precision=2))
 
+    store_id = db.Column(db.Integer, db.ForeignKey('stores.id'))
+    store = db.relationship('StoreModel')
+
     def __init__(self, name, price):
         self.name = name
         self.price = price
+        self.store_id = store_id
 
     def json(self):
         return {'name': self.name, 'price': self.price}
@@ -22,7 +26,6 @@ class ItemModel(db.Model):
     def save_to_db(self):
         db.session.add(self)
         db.session.commit()
-
 
     def delete_from_db(self):
         db.session.delete(self)
