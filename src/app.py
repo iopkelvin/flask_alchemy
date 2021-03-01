23a25e4c4edf29666595c411e78f6f2fc4f0b8a0
Kelvin Ortiz <36288868+iopkelvin@users.noreply.github.com>
2021-02-28 18:32:26 -0800

    modified run.py and location of dbimportdb

--- src/app.py
+++ src/app.py
@@ -7,8 +7,6 @@ from resources.user import UserRegister
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
 
-from db import db
-
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 # Turns off Flask SQL Alchemy modification tracker, not the library's
@@ -27,5 +25,6 @@ api.add_resource(ItemList, '/items')
 api.add_resource(UserRegister, '/register')
 
 if __name__ == '__main__':
+    from db import db
     db.init_app(app)
     app.run(port=5000, debug=True)  # debug for error messages (html page)
